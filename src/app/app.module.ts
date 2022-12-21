29a6368ff449857c3a0a58d7f812a509b0622cb9
peterson-perez <p.perez60@unapec.edu.do>
2022-12-21 21:56:01 +0100

    feat:  httpClientModule was added and CommonModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,25 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule,FormsModule } from '@angular/forms';
+import { ReactiveFormsModule,FormsModule, FormControl } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule, Pages } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AddproductComponent } from './components/addproduct/addproduct.component';
-import { EditproductComponent } from './components/editproduct/editproduct.component';
-import { DeleteproductComponent } from './components/deleteproduct/deleteproduct.component';
-import { ListallproductsComponent } from './components/listallproducts/listallproducts.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Pages,
-    AddproductComponent,
-    EditproductComponent,
-    DeleteproductComponent,
-    ListallproductsComponent
+    Pages
 
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
