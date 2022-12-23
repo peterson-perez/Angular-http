76abeeaaf85bc807190b213e990439c539acad6a
peterson-perez <p.perez60@unapec.edu.do>
2022-12-23 17:27:16 +0100

    feat: the logic for get products all, and nagivate

--- src/app/components/listallproducts/listallproducts.component.ts
+++ src/app/components/listallproducts/listallproducts.component.ts
@@ -1,15 +1,38 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ProductI } from 'src/app/Models/ModelProduct';
+import { ServiciosService } from 'src/app/servicios.service';
 
 @Component({
   selector: 'app-listallproducts',
   templateUrl: './listallproducts.component.html',
   styleUrls: ['./listallproducts.component.css']
 })
-export class ListallproductsComponent implements OnInit {
 
-  constructor() { }
+export class ListAllproductsComponent implements OnInit {
+
+  public products: ProductI[] = []
+
+  constructor(private service:ServiciosService, private routes: Router) { 
+  }
 
   ngOnInit(): void {
+    this.service.getProductAll().subscribe( data => {
+      this.products = data
+      console.log(this.products)
+    })
+  }
+
+  onSubmit = () => {
+    this.routes.navigate(['/AddProduct'])
+  }
+
+   edit = () => {
+    this.routes.navigate(['/editProduct'])
+  }
+
+  delete = () => {
+
   }
 
 }
