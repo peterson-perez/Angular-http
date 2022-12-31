a3bed611073b3fcd7bee5c5bf5c246c6cd9551ae
peterson-perez <p.perez60@unapec.edu.do>
2022-12-31 11:48:03 +0100

    feat: id in the navigate and lenghtProducts

--- src/app/components/listallproducts/listallproducts.component.ts
+++ src/app/components/listallproducts/listallproducts.component.ts
@@ -2,6 +2,7 @@ import { Component, Injectable, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { ProductI } from 'src/app/Models/ModelProductInt';
 import { ServiciosService } from 'src/app/servicios.service';
+import { DeleteproductComponent } from '../deleteproduct/deleteproduct.component';
 
 @Component({
   selector: 'app-listallproducts',
@@ -11,28 +12,42 @@ import { ServiciosService } from 'src/app/servicios.service';
 
 export class ListAllproductsComponent implements OnInit {
 
-  public products: ProductI[] = []
+  public products: ProductI[] = [];
+  public lenghtProducts: number = 0;
 
-  constructor(private service:ServiciosService, private routes: Router) { 
-  }
+  
+  constructor(private service: ServiciosService, 
+    private routes: Router, 
+    private deleteProduct: DeleteproductComponent) {}
 
   ngOnInit(): void {
-    this.service.getProductAll().subscribe( data => {
+    this.service.getProductAll().subscribe(data => {
       this.products = data
       console.log(this.products)
+       this.lenghtProducts = this.products.length
+      console.log(this.lenghtProducts)
     })
+
+
   }
 
   onSubmit = () => {
     this.routes.navigate(['/AddProduct'])
   }
 
-   edit = (id: number) => {
-    this.routes.navigate(['/editProduct'])
+  edit = (id: number) => {
+    this.routes.navigate(['/editProduct', id])
   }
 
   delete = (id: number) => {
-
+    this.products.find((product) => {
+      if (product.id === id) {
+        this.service.deleteProduct(id).subscribe( data => {
+          this.products = this.products.filter(products => products.id != id)
+          return this.products   
+        })
+      }
+    })
   }
 
 }
