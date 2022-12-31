6a57237558cb2b2551cb471b5a78ede9f9c9bf26
peterson-perez <p.perez60@unapec.edu.do>
2022-12-31 11:53:48 +0100

    feat: service to getProductId, getProductByName, putProduct

--- src/app/servicios.service.ts
+++ src/app/servicios.service.ts
@@ -1,9 +1,10 @@
 import { Injectable } from '@angular/core';
-import { map, Observable } from 'rxjs';
+import { map, Observable, ObservedValueOf } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { ProductI } from './Models/ModelProductInt';
 import { Product } from './Models/ModelProduct';
+import { EditProductI } from './Models/ModelEditProducts';
 
 
 
@@ -29,9 +30,27 @@ export class ServiciosService {
    return this.http.get<ProductI[]>("/api/ProductAll")
   }
 
+  // get Product id
+
+  getproductId(id: number): Observable<ProductI>{
+    return this.http.get<ProductI>('/api/ProductId?id=' + id)
+  }
+
+  getProductsByName(name: any): Observable<ProductI>{
+    return this.http.get<ProductI>('/api/ProductName?name=' + name)
+  }
+
   // delete
 
   deleteProduct(id: number): Observable<ProductI[]>{
-    return this.http.delete<ProductI[]>('/api/ProductId' + id)
+    return this.http.delete<ProductI[]>('/api/ProductId?id=' + id)
   }
+
+  //edit
+  
+  putProduct(id: number, form: EditProductI): Observable<ProductI>{
+    return this.http.put<ProductI>('/api/ProductId?id=' + id, form)
+  }
+
 }
+
