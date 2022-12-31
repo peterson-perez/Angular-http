4edb0d635e6ee20294df4f7d2ad50f98aa050733
peterson-perez <p.perez60@unapec.edu.do>
2022-12-31 11:52:24 +0100

    feat: function deleteProduct

--- src/app/components/deleteproduct/deleteproduct.component.ts
+++ src/app/components/deleteproduct/deleteproduct.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injectable, OnInit } from '@angular/core';
+import { ProductI } from 'src/app/Models/ModelProductInt';
+import { ServiciosService } from 'src/app/servicios.service';
 
 @Component({
   selector: 'app-deleteproduct',
   templateUrl: './deleteproduct.component.html',
   styleUrls: ['./deleteproduct.component.css']
 })
+
+@Injectable({providedIn: 'root'})
+
 export class DeleteproductComponent implements OnInit {
 
-  constructor() { }
+  products: ProductI[] = [];
+
+  constructor(private service: ServiciosService) { }
 
   ngOnInit(): void {
   }
+
+  deleteProduct(id: number) {
+    this.products.find((product) => {
+      if (product.id === id) {
+        this.service.deleteProduct(id).subscribe( data => {
+          this.products = this.products.filter(products => products.id != id)
+          return this.products   
+        })
+      }
+    })
+   }
 
 }
